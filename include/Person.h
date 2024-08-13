a656c64807bba4e842f64ff52dcb52515543a62c
ahmedalaa513 <ahmed610alaa56@gmail.com>
2024-08-13 10:17:53 +0300

    Update Person.h

--- include/Person.h
+++ include/Person.h
@@ -5,18 +5,22 @@ using namespace std;
 
 class Person {
 protected:
+    // ================Attriputes===================
     string name;
     string password;
     int id;
 public:
+    // ============default constructor==============
     Person(){
         id = 0;
     }
+    // ==========parameterized constructor==========
     Person(string name,string password , int id){
         this->name = name;
         this->password = pw;
         this->id = id;
     }
+    // ==================Setters====================
     void setName(string name){
         this->name = name;
     }
@@ -26,6 +30,7 @@ public:
     void setId(int id){
         this->id = id;
     }
+    // =================Getters====================
     string getName(){
         return name;
     }
@@ -35,6 +40,7 @@ public:
     int getId(){
         return id;
     }
+    // ================Method======================
     virtual void Display() = 0;
 };
 
